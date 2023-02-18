b9bdb759faee659df44348165685cc4e5fe6859a
o3x1998 <48043951+o3x1998@users.noreply.github.com>
2023-02-18 21:26:54 +0900

    [Silver V] Title: 폴리오미노, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.py
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.py
@@ -1,33 +1,47 @@
-p1 = "AAAA"
-p2 = "BB"
+# p1 = "AAAA"
+# p2 = "BB"
 board = input()
-result = []
-cnt = 0
-fail = 0
+# result = []
+# cnt = 0
+# fail = 0
 
-def change(cnt):
-    a = cnt // 4
-    b = (cnt % 4) // 2
+# def change(cnt):
+#     a = cnt // 4
+#     b = (cnt % 4) // 2
     
-    if (cnt % 4) % 2 > 0:
-        return 1
+#     if (cnt % 4) % 2 > 0:
+#         return 1
+#     else:
+#         result.append(p1*a)
+#         result.append(p2*b)
+#         return 0
+
+# for x in board:
+#     if x == '.':
+#         fail = change(cnt)
+#         if fail: 
+#             break
+#         result.append(x)
+#         cnt = 0
+#         continue
+#     cnt += 1
+# fail = change(cnt)
+
+# if fail:
+#     print("-1")
+# else:
+#     print(''.join(result))
+
+
+
+
+def sol(board):
+    board = board.replace('XXXX', 'AAAA')
+    board = board.replace('XX', 'BB')
+
+    if board.count('X') != 0:
+        return -1
     else:
-        result.append(p1*a)
-        result.append(p2*b)
-        return 0
+        return board
 
-for x in board:
-    if x == '.':
-        fail = change(cnt)
-        if fail: 
-            break
-        result.append(x)
-        cnt = 0
-        continue
-    cnt += 1
-fail = change(cnt)
-
-if fail:
-    print("-1")
-else:
-    print(''.join(result))
+print(sol(board))
