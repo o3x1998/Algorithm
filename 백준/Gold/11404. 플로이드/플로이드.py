6d63640c711bf82e6409b0a8f4ef3d8ca9b5b1ea
o3x1998 <48043951+o3x1998@users.noreply.github.com>
2023-02-26 13:18:38 +0900

    [Gold IV] Title: 플로이드, Time: 640 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,28 +1,24 @@
 import sys
+input = sys.stdin.readline
 
-n = int(sys.stdin.readline())
-m = int(sys.stdin.readline())
+n = int(input())
+m = int(input())
 
 bus_cost = [[float('inf')] * (n+1) for _ in range(n+1)]
              
 for _ in range(m):
-  s, e, c = map(int, sys.stdin.readline().split())
+  s, e, c = map(int, input().split())
   bus_cost[s][e] = min(c, bus_cost[s][e])
 
 # 플로이드-워셜 알고리즘
 for k in range(1, n+1): # k는 어느 정류장을 거쳐갈건지. 경로 for문이 가장 상위 단계여야 누락되지 않는다
-  for i in range(1, n+1):
-    for j in range(1, n+1):
-      if i == j: # 자기 자신으로 오는 경우는 없다고 함
-        bus_cost[i][j] = 0
-      else: # 경로 거치는 것 or 직접 가는 것 or 이전 경로들
-        bus_cost[i][j] = min(bus_cost[i][j], bus_cost[i][k] + bus_cost[k][j])
+    bus_cost[k][k] = 0 # 시작도시와 도착도시가 같은 경우는 없다
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            bus_cost[i][j] = min(bus_cost[i][j], bus_cost[i][k] + bus_cost[k][j])
 
-for i in range(1, n+1) :
-  for j in range(1, n+1):
-    if bus_cost[i][j] == float('inf'):
-      print(0, end = " ")
-    else:
-      print(bus_cost[i][j], end = " ")
-  print()
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        print(bus_cost[i][j] if bus_cost[i][j] != float('inf') else 0, end = " ")
+    print()
   
