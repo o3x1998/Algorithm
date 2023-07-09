89cf3511a66fa9e0555bdb6129fa86dd94c4528e
o3x1998 <48043951+o3x1998@users.noreply.github.com>
2023-07-09 21:30:50 +0900

    [level 2] Title: 타겟 넘버, Time: 1.75 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,10 +1,7 @@
 from itertools import product
 
 def solution(numbers, target):
-    answer = 0
-    tmp = list(product([-1, 1], repeat=len(numbers)))
-    for i in range(len(tmp)):
-        mul = [x*y for x,y in zip(numbers, tmp[i])]
-        if sum(mul) == target: answer += 1
-    
-    return answer
+    a = [(-n, n) for n in numbers]
+    b = list(map(sum, product(*a)))
+
+    return b.count(target)
